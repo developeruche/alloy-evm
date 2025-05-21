0e479b319c19e02b4fa1281c7acca9f1a29773b1
developeruche <developeruche@gmail.com>
2025-05-21 14:58:31 +0100

    added EthHybridEvm

--- crates/evm/src/eth/mod.rs
+++ crates/evm/src/eth/mod.rs
@@ -7,6 +7,7 @@ use core::{
     fmt::Debug,
     ops::{Deref, DerefMut},
 };
+use hybrid_evm::evm::HybridEvm;
 use revm::{
     context::{BlockEnv, CfgEnv, Evm as RevmEvm, TxEnv},
     context_interface::result::{EVMError, HaltReason, ResultAndState},
@@ -42,6 +43,17 @@ pub struct EthEvm<DB: Database, I, PRECOMPILE = EthPrecompiles> {
         EthInstructions<EthInterpreter, EthEvmContext<DB>>,
         PRECOMPILE,
     >,
+    inspect: bool,
+}
+
+/// Ethereum hybrid EVM implementation.
+///
+/// This is a wrapper type around the `revm` ethereum evm with optional [`Inspector`] (tracing)
+/// support. [`Inspector`] support is configurable at runtime because it's part of the underlying
+/// [`RevmEvm`] type.
+#[expect(missing_debug_implementations)]
+pub struct EthHybridEvm<DB: Database, I> {
+    inner: HybridEvm<EthEvmContext<DB>, I>,
     inspect: bool,
 }
 
@@ -81,6 +93,31 @@ impl<DB: Database, I, PRECOMPILE> EthEvm<DB, I, PRECOMPILE> {
     }
 }
 
+impl<DB: Database, I> EthHybridEvm<DB, I> {
+    /// Creates a new Ethereum EVM instance.
+    ///
+    /// The `inspect` argument determines whether the configured [`Inspector`] of the given
+    /// [`RevmEvm`] should be invoked on [`Evm::transact`].
+    pub const fn new(evm: HybridEvm<EthEvmContext<DB>, I>, inspect: bool) -> Self {
+        Self { inner: evm, inspect }
+    }
+
+    /// Consumes self and return the inner EVM instance.
+    pub fn into_inner(self) -> HybridEvm<EthEvmContext<DB>, I> {
+        self.inner
+    }
+
+    /// Provides a reference to the EVM context.
+    pub const fn ctx(&self) -> &EthEvmContext<DB> {
+        &self.inner.0.ctx
+    }
+
+    /// Provides a mutable reference to the EVM context.
+    pub fn ctx_mut(&mut self) -> &mut EthEvmContext<DB> {
+        &mut self.inner.0.ctx
+    }
+}
+
 impl<DB: Database, I, PRECOMPILE> Deref for EthEvm<DB, I, PRECOMPILE> {
     type Target = EthEvmContext<DB>;
 
@@ -90,7 +127,23 @@ impl<DB: Database, I, PRECOMPILE> Deref for EthEvm<DB, I, PRECOMPILE> {
     }
 }
 
+impl<DB: Database, I> Deref for EthHybridEvm<DB, I> {
+    type Target = EthEvmContext<DB>;
+
+    #[inline]
+    fn deref(&self) -> &Self::Target {
+        self.ctx()
+    }
+}
+
 impl<DB: Database, I, PRECOMPILE> DerefMut for EthEvm<DB, I, PRECOMPILE> {
+    #[inline]
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.ctx_mut()
+    }
+}
+
+impl<DB: Database, I> DerefMut for EthHybridEvm<DB, I> {
     #[inline]
     fn deref_mut(&mut self) -> &mut Self::Target {
         self.ctx_mut()
@@ -219,6 +272,130 @@ where
 
     fn inspector_mut(&mut self) -> &mut Self::Inspector {
         &mut self.inner.inspector
+    }
+}
+
+impl<DB, I> Evm for EthHybridEvm<DB, I>
+where
+    DB: Database,
+    I: Inspector<EthEvmContext<DB>>,
+{
+    type DB = DB;
+    type Tx = TxEnv;
+    type Error = EVMError<DB::Error>;
+    type HaltReason = HaltReason;
+    type Spec = SpecId;
+    type Precompiles = EthPrecompiles;
+    type Inspector = I;
+
+    fn block(&self) -> &BlockEnv {
+        &self.block
+    }
+
+    fn chain_id(&self) -> u64 {
+        self.cfg.chain_id
+    }
+
+    fn transact_raw(&mut self, tx: Self::Tx) -> Result<ResultAndState, Self::Error> {
+        if self.inspect {
+            self.inner.set_tx(tx);
+            self.inner.inspect_replay()
+        } else {
+            self.inner.transact(tx)
+        }
+    }
+
+    fn transact_system_call(
+        &mut self,
+        caller: Address,
+        contract: Address,
+        data: Bytes,
+    ) -> Result<ResultAndState, Self::Error> {
+        let tx = TxEnv {
+            caller,
+            kind: TxKind::Call(contract),
+            // Explicitly set nonce to 0 so revm does not do any nonce checks
+            nonce: 0,
+            gas_limit: 30_000_000,
+            value: U256::ZERO,
+            data,
+            // Setting the gas price to zero enforces that no value is transferred as part of the
+            // call, and that the call will not count against the block's gas limit
+            gas_price: 0,
+            // The chain ID check is not relevant here and is disabled if set to None
+            chain_id: None,
+            // Setting the gas priority fee to None ensures the effective gas price is derived from
+            // the `gas_price` field, which we need to be zero
+            gas_priority_fee: None,
+            access_list: Default::default(),
+            // blob fields can be None for this tx
+            blob_hashes: Vec::new(),
+            max_fee_per_blob_gas: 0,
+            tx_type: 0,
+            authorization_list: Default::default(),
+        };
+
+        let mut gas_limit = tx.gas_limit;
+        let mut basefee = 0;
+        let mut disable_nonce_check = true;
+
+        // ensure the block gas limit is >= the tx
+        core::mem::swap(&mut self.block.gas_limit, &mut gas_limit);
+        // disable the base fee check for this call by setting the base fee to zero
+        core::mem::swap(&mut self.block.basefee, &mut basefee);
+        // disable the nonce check
+        core::mem::swap(&mut self.cfg.disable_nonce_check, &mut disable_nonce_check);
+
+        let mut res = self.transact(tx);
+
+        // swap back to the previous gas limit
+        core::mem::swap(&mut self.block.gas_limit, &mut gas_limit);
+        // swap back to the previous base fee
+        core::mem::swap(&mut self.block.basefee, &mut basefee);
+        // swap back to the previous nonce check flag
+        core::mem::swap(&mut self.cfg.disable_nonce_check, &mut disable_nonce_check);
+
+        // NOTE: We assume that only the contract storage is modified. Revm currently marks the
+        // caller and block beneficiary accounts as "touched" when we do the above transact calls,
+        // and includes them in the result.
+        //
+        // We're doing this state cleanup to make sure that changeset only includes the changed
+        // contract storage.
+        if let Ok(res) = &mut res {
+            res.state.retain(|addr, _| *addr == contract);
+        }
+
+        res
+    }
+
+    fn db_mut(&mut self) -> &mut Self::DB {
+        &mut self.journaled_state.database
+    }
+
+    fn finish(self) -> (Self::DB, EvmEnv<Self::Spec>) {
+        let Context { block: block_env, cfg: cfg_env, journaled_state, .. } = self.inner.0.ctx;
+
+        (journaled_state.database, EvmEnv { block_env, cfg_env })
+    }
+
+    fn set_inspector_enabled(&mut self, enabled: bool) {
+        self.inspect = enabled;
+    }
+
+    fn precompiles(&self) -> &Self::Precompiles {
+        &self.inner.0.precompiles
+    }
+
+    fn precompiles_mut(&mut self) -> &mut Self::Precompiles {
+        &mut self.inner.0.precompiles
+    }
+
+    fn inspector(&self) -> &Self::Inspector {
+        &self.inner.0.inspector
+    }
+
+    fn inspector_mut(&mut self) -> &mut Self::Inspector {
+        &mut self.inner.0.inspector
     }
 }
 
